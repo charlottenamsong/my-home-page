7ad110160904db1b6e712514d72679ca2968b161
Charlotte Namsong <charlottenamsong@gmail.com>
2020-03-21 13:15:24 +0700

    menu

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React, { useState } from "react";
 import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { Row, Col, Menu } from "antd";
 import {
+  HomeOutlined,
   MailOutlined,
   AppstoreOutlined,
   SettingOutlined
 } from "@ant-design/icons";
 
 const { SubMenu } = Menu;
+
+const Home = () => {
+  return <div>Home</div>;
+}
+
+const About = () => {
+  return <div>
+    About
+  </div>;
+}
 
 function App() {
   const [state, setState] = useState({
@@ -31,23 +43,23 @@ function App() {
             mode="horizontal"
           >
             <Menu.Item key="mail">
-              <MailOutlined />
-              Navigation One
+              <HomeOutlined />
+              <a href="/">Charlotte Namsong</a>
             </Menu.Item>
             <Menu.Item key="app" disabled>
               <AppstoreOutlined />
-              Navigation Two
+              App
             </Menu.Item>
             <SubMenu
               title={
                 <span className="submenu-title-wrapper">
                   <SettingOutlined />
-                  Navigation Three - Submenu
+                  Settings
                 </span>
               }
             >
               <Menu.ItemGroup title="Item 1">
-                <Menu.Item key="setting:1">Option 1</Menu.Item>
+                <Menu.Item key="setting:1"><MailOutlined />Option 1</Menu.Item>
                 <Menu.Item key="setting:2">Option 2</Menu.Item>
               </Menu.ItemGroup>
               <Menu.ItemGroup title="Item 2">
@@ -55,16 +67,17 @@ function App() {
                 <Menu.Item key="setting:4">Option 4</Menu.Item>
               </Menu.ItemGroup>
             </SubMenu>
-            <Menu.Item key="alipay">
-              <a
-                href="https://ant.design"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Navigation Four - Link
-              </a>
+            <Menu.Item key="about">
+              <a href="/about">About</a>
             </Menu.Item>
           </Menu>
+
+          <BrowserRouter>
+            <Switch>
+              <Route path="/" component={Home} exact />
+              <Route path="/about" component={About} />
+            </Switch>
+          </BrowserRouter>
         </Col>
       </Row>
     </>
